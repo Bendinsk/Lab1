d3606bb65cd02e141e7ed5d9756db9596cc2c305
Bendinsk <joremax2002@gmail.com>
2023-10-02 15:40:49 +0300

    Pakeitimai

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,11 +1,26 @@
 package com.example.myapplication
 
-import androidx.appcompat.app.AppCompatActivity
+import android.graphics.Color
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
 
 class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
+    var tv: TextView? = null
+    var btnTextChange: Button? = null
+    var btnColorChange: Button? = null
+    fun onBtnChangeTextClick(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        tv = findViewById<View>(R.id.tvMain) as TextView
+        btnTextChange = findViewById<View>(R.id.BtnChangeText) as Button
+        btnColorChange = findViewById<View>(R.id.BtnChangeText) as Button
+        btnTextChange!!.setOnClickListener { tv!!.text = "Mygtukas yra paspaustas" }
+        btnColorChange!!.setOnClickListener { tv!!.setTextColor(Color.parseColor("#FF0000")) }
     }
 }
+
+
